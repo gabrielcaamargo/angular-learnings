b25d4ecb1758fe9d86f10e77db042f1f977a63ea
gaabrielcamargo <gcamargo.dev@gmail.com>
2022-05-16 23:20:02 -0300

    passed title with interpolation

--- data-biding/src/app/data-binding/data-binding.component.ts
+++ data-biding/src/app/data-binding/data-binding.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./data-binding.component.css']
 })
 export class DataBindingComponent implements OnInit {
-
+  InterpolationTitle:string = 'Interpolation'
+  PropertyBindingTitle:string = 'Property Binding'
   url:string = 'http://loiane.com';
   cursoAngular:boolean = true;
   urlImagem:string = 'http://lorempixel.com.br/400/200/'
